a938fb9076850a52a3209a5f9ebe4673a6ee0278
Danzolax <danzolax@gmail.com>
2021-01-26 13:35:52 +0300

    add sqldump

---
+++ src/ru/zolax/callapp/util/DiscountType.java
@@ -0,0 +1,10 @@
+package ru.zolax.callapp.util;
+
+public class DiscountType {
+    public static final int FROM_0_TO_5 = 1;
+    public static final int FROM_5_TO_15 = 2;
+    public static final int FROM_15_TO_30 = 3;
+    public static final int FROM_30_TO_70 = 4;
+    public static final int FROM_70_TO_100 = 5;
+    public static final int NO_DISCOUNT = 6;
+}
--- src/ru/zolax/callapp/util/SqlDump.java
+++ src/ru/zolax/callapp/util/SqlDump.java
@@ -4,9 +4,7 @@ import ru.zolax.callapp.Application;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.TimerTask;
 
 public class SqlDump extends TimerTask {
